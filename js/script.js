91b28bb75ea669181fc9cca1a3d92953b2f9b7e1
Camsbury <camsbury7@gmail.com>
2014-08-02 10:09:48 -0400

    Reset to defined width and constant dimensions (give or take a square at higher dimensions)

--- js/script.js
+++ js/script.js
@@ -8,5 +8,24 @@ $(document).ready(function(){
     });
 
 function reset(){
+    var g=prompt('What should the side length (in squares) of the grid be?');
+    if (isNaN(g)) {
+        var g=prompt('Sorry, that wasn\'t a number. What should the side length (in squares) of the grid be?');
+    }
+    $('tbody').empty();
+    for (var i=1;i<=g;i++) {
+        $('tbody').append('<tr></tr>')
+    }
+    for (var i=1;i<=g;i++) {
+        $('tbody tr').append('<td><div></div></td>')
+    }
+    $('td div').height(Math.ceil(320/g)+'px');
+    $('td div').width(Math.ceil(320/g)+'px');
     $('td div').css('background-color','lightblue');
+    $('div').mouseenter(function(){
+        $(this).css('background-color','green');
+        });
+    $('div').mouseleave(function(){
+        $(this).css('background-color','blue');
+        });            
 }
